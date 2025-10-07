ebe6e572b1158a7853936db7b43ce41d72d30d4f
ai159357-spc <aia669802@gmail.com>
2025-10-07 16:23:28 +0530

    sdsfds

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Adddata from "./Adddata";
 import Display from "./Display";
-import Navbar from "./Navbar";
-
-
+import Navbar from "./navbar";
 import "./App.css";
 
 const App = () => {
--- src/Display.jsx
+++ src/Display.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { collection, getDocs } from "firebase/firestore";
-import { db } from './firebase/firebase';
+import { db } from './firebase data/firbase';
 import "./App.css";
 
 const Display = () => {
